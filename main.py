6225a4261aa2cea3e3de19640299f9b68805363b
Dinesh Babu <dinesh.babu1408@gmail.com>
2024-07-19 11:15:25 -0400

    ticket 1

--- main.py
+++ main.py
@@ -35,3 +35,48 @@ def encoderFn(rotationint, encode):
     print("The encoded string is: " + encodedstr)
     print("")
     return encodedstr #returns the encoded string
+
+while start not in commands:  # keeps prompting user until they enter a command in the bank
+    start = input("e - encode a string\nd - decode a string\nq - quit\n\n")
+    if start == "e":
+        while encode == "":  # keeps prompting user until they type a lowercase letter from alphabet list
+            encode = input("What string would you like to encode?\n")
+        # keeps prompting user until their int is in range
+        while rotationint < 1 or rotationint > 25:
+            rotationint = int(input("Pick a rotation integer in the range of 1-25:\n"))
+        encoderFn(rotationint, encode)
+
+    elif start == "d":
+        new = []
+        alphabet = list(string.ascii_lowercase)
+        numbers = list(range(26))  # set list of numbers from 0 to 25
+        tomod = list(range(26))  # list that will undergo slicing and changes
+        rotation = 0
+
+        decode = input("What string would you like to decode?\n")
+        decode = decode.lower()
+        plaintext = input("Enter a decoded word from the string:\n")
+        plaintext = plaintext.lower()
+        encode = plaintext
+        enclist = []
+        foundrot = 0
+
+        for i in range(1, 26):
+            x = []
+            tomod = list(range(26))
+            del tomod[-i:]
+            endslice = numbers[-i:]
+            new = endslice + tomod
+
+            for i in encode:
+                if i in alphabet:
+                    x.append(alphabet.index(i))  # index of input letters in alphabet list
+                else:
+                    x.append(i)  # if it is a non alphabet character
+
+            for i in range(len(x)):
+                if isinstance(x[i], int):  # if the index value i of x is an integer...
+                    x[i] = alphabet[new[x[i]]]
+            encodedstr = "".join(x)  # takes the list x and turns it into a str
+            enclist.append(encodedstr)
+        declist = decode.split()
