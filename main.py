ad1b038c06d283e9fc536e7d49ffbb9aec340c0c
Dinesh Babu <dinesh.babu1408@gmail.com>
2024-07-19 09:59:31 -0400

    ticket 1

--- main.py
+++ main.py
@@ -19,4 +19,19 @@ print("Welcome to Caesar Cipher!")
 def encoderFn(rotationint, encode):
     alphabet = list(string.ascii_lowercase)  # list of alphabet in lowercase
     x = []  # letters in input
-    del tomod[-rotationint:] #takes duplicated list of number and deletes numbers from the right by the # of
+    del tomod[-rotationint:] #takes duplicated list of number and deletes numbers from the right by the # ofrotationint
+    endslice = numbers[-rotationint:]
+    new = endslice + tomod
+    for i in encode:
+        if i in alphabet:
+            x.append(alphabet.index(i))  # index of input letters in alphabet list
+        else:
+            x.append(i)  # if it is a non alphabet character
+
+    for i in range(len(x)): #for the number of values in list x
+        if isinstance(x[i], int):  # if the index value i of x is an integer...
+            x[i] = alphabet[new[x[i]]]
+    encodedstr = "".join(x)  # takes the list x and turns it into a str
+    print("The encoded string is: " + encodedstr)
+    print("")
+    return encodedstr #returns the encoded string
